f4398be017b3f21eec3d79114a5dd19af7270320
Morten <me@mbork.dk>
2017-10-03 10:27:23 +0200

    Chaged header to setHeader

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use('/new-workout', newWorkout);
 //API
 app.use('/api', indexApi);
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
